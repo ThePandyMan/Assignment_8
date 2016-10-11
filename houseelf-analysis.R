726fa28a0142231910677142a2118bc0817457ba
Andrew Baird <Abaird42@gmail.com>
2016-10-10 20:10:04 -0500

    added function to calculate GC content

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -1,2 +1,17 @@
 # To fix the hard work that was lost
-data <- read.csv('houseelf_earlength_dna_data_1.csv')
+data <- data.frame(read.csv('houseelf_earlength_dna_data_1.csv'))
+
+#Libraries
+library(stringr)
+
+# --Pushing Changes--
+
+gc_content <- function(dnaseq) {
+  l <- str_to_lower(dnaseq)
+  t <- str_length(l)
+  g <- str_count(l, "g")
+  c <- str_count(l, "c")
+  gc <- round((g + c) / t * 100, 2)
+  return(gc)}
+
+gc_content(data$dnaseq)
