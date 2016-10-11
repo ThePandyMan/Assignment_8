54a87ad9940738273dd827c441e50d03fd3a7506
Andrew Baird <Abaird42@gmail.com>
2016-10-10 21:04:45 -0500

    finished up the analysis

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -15,3 +15,34 @@ gc_content <- function(dnaseq) {
   return(gc)}
 
 gc_content(data$dnaseq)
+
+# --Pushing and Pulling--
+
+get_size_class <- function(ear_length){
+  # Calculate the classification for multiple ear lengths
+  ear_lengths <- ifelse(ear_length > 10, "large", "small")
+  return(ear_lengths)
+}
+
+# ID, ear-class,and GC-content output as a Data frame
+output_table <- data.frame(data$id, ear_size = NA, gc_content = NA)
+
+for (i in 1:length(data$earlength)) {
+  if (data$earlength[i] > 10) 
+  {output_table$ear_size[i] <- "LARGE"
+   } else {output_table$ear_size[i] <- "SMALL"}}
+
+
+gc_content <- function(elf) {
+  t <- str_length(elf)
+  g <- str_count(elf, "g")
+  c <- str_count(elf, "c")
+  gc <- round((g + c) / t * 100, 2)}
+
+for (i in 1:length(data$dnaseq)) {
+  lower <- tolower(data$dnaseq)
+  output_table$gc_content[i] <- gc_content(lower[i])}
+
+print(output_table)
+
+write.csv(output_table, file = "houseelf-analysis.csv")
